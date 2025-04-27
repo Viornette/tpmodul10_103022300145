dd1e15bb697d3651047943e80aabfdf6744627aa
Viornette <easonluciel@gmail.com>
2025-04-28 00:13:48 +0700

    versi awal

---
+++ tp10_103022300145/Controllers/MahasiswaController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace tp10_103022300145.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MahasiswaController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<MahasiswaController> _logger;
+
+    public MahasiswaController(ILogger<MahasiswaController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<Mahasiswa> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Mahasiswa
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
